ebc18c2e131e35411a3837a6df68f3e02b0d0a5a
Erik Saarenvirta <erik.saarenvirta@slalom.com>
2018-04-03 17:35:20 -0400

    got big query insert workinggit add -A!

--- core/src/main/java/org/plasma/core/PlasmaBigQueryTools.java
+++ core/src/main/java/org/plasma/core/PlasmaBigQueryTools.java
@@ -10,6 +10,10 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.URISyntaxException;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
 import java.util.UUID;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -24,19 +28,24 @@ import com.google.api.services.bigquery.Bigquery;
 import com.google.api.services.bigquery.BigqueryScopes;
 import com.google.api.services.bigquery.model.Dataset;
 import com.google.api.services.bigquery.model.DatasetReference;
+import com.google.api.services.bigquery.model.TableRow;
 import com.google.auth.Credentials;
 import com.google.auth.oauth2.GoogleCredentials;
 import com.google.auth.oauth2.ServiceAccountCredentials;
 import com.google.cloud.bigquery.BigQuery;
+import com.google.cloud.bigquery.BigQueryError;
 import com.google.cloud.bigquery.BigQueryOptions;
 import com.google.cloud.bigquery.DatasetInfo;
 import com.google.cloud.bigquery.FieldValueList;
+import com.google.cloud.bigquery.InsertAllRequest;
+import com.google.cloud.bigquery.InsertAllResponse;
 import com.google.cloud.bigquery.Job;
 import com.google.cloud.bigquery.JobException;
 import com.google.cloud.bigquery.JobId;
 import com.google.cloud.bigquery.JobInfo;
 import com.google.cloud.bigquery.QueryJobConfiguration;
 import com.google.cloud.bigquery.QueryResponse;
+import com.google.cloud.bigquery.TableId;
 import com.google.cloud.bigquery.TableResult;
 
 public class PlasmaBigQueryTools {
@@ -83,6 +92,28 @@ public class PlasmaBigQueryTools {
 		for (com.google.cloud.bigquery.Dataset dataset : bqconn.listDatasets().iterateAll()) {
 		    System.out.printf("%s%n", dataset.getDatasetId().getDataset());
 		}
+		
+		TableId tableId = TableId.of("plasma_reddit", "test_table");
+		// Values of the row to insert
+		Map<String, Object> rowContent = new HashMap<>();
+		InsertAllRequest.Builder request = InsertAllRequest.newBuilder(tableId);
+		for (int i = 0; i < 10; i++) {
+			rowContent.put("field1", Integer.toString(i));
+			rowContent.put("field2", Double.toString(Math.pow(i, 2)));
+			request = request.addRow(rowContent);
+		}
+		
+		InsertAllResponse response = bqconn.insertAll(request.build());
+		
+		
+		
+		if (response.hasErrors()) {
+		  // If any of the insertions failed, this lets you inspect the errors
+		  for (Entry<Long, List<BigQueryError>> entry : response.getInsertErrors().entrySet()) {
+		    // inspect row error
+		  }
+		}
+		
 	}
 
 }
