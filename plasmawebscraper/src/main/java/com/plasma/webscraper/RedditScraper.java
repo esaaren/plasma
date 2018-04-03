ff6913be17737c09a3889b7f2e2738e86978433c
Erik Saarenvirta <erik.saarenvirta@slalom.com>
2018-04-03 09:13:13 -0400

    Added the scraper project in netbeans, backed up chrome bookmarks

---
+++ plasmawebscraper/src/main/java/com/plasma/webscraper/RedditScraper.java
@@ -0,0 +1,21 @@
+package com.plasma.webscraper;
+
+// Imports the Google Cloud client library
+import com.google.cloud.bigquery.BigQuery;
+import com.google.cloud.bigquery.BigQueryOptions;
+import com.google.cloud.bigquery.FieldValueList;
+import com.google.cloud.bigquery.Job;
+import com.google.cloud.bigquery.JobId;
+import com.google.cloud.bigquery.JobInfo;
+import com.google.cloud.bigquery.QueryJobConfiguration;
+import com.google.cloud.bigquery.QueryResponse;
+import com.google.cloud.bigquery.TableResult;
+import java.util.UUID;
+
+public class RedditScraper {
+
+    public static void main(String[] args) {
+        
+    }
+    
+}
