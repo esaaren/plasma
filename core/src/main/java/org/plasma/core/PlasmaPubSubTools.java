77f5bf096db68d423540938c051f96336e1bc9bd
Erik Saarenvirta <erik.saarenvirta@slalom.com>
2018-04-20 13:14:30 -0400

    Got reading from pubsub enough to make debug work

--- core/src/main/java/org/plasma/core/PlasmaPubSubTools.java
+++ core/src/main/java/org/plasma/core/PlasmaPubSubTools.java
@@ -22,81 +22,100 @@ import com.google.pubsub.v1.ProjectTopicName;
 import com.google.pubsub.v1.PubsubMessage;
 
 public class PlasmaPubSubTools {
+	
+	public static GoogleCredentials getCredentials() {
+		
+		GoogleCredentials credentials = null;
+	    File credentialsPath = new File("C:\\PersonalWork\\files\\pubsub_key.json");  // Key path
+	    try (FileInputStream serviceAccountStream = new FileInputStream(credentialsPath)) {
+	        credentials = ServiceAccountCredentials.fromStream(serviceAccountStream);
+	    } catch (FileNotFoundException ex) {
+		  Logger.getLogger(PlasmaPubSubTools.class.getName()).log(Level.SEVERE, "File not found", ex);
+		 
+	    } catch (IOException ex) {
+		  Logger.getLogger(PlasmaPubSubTools.class.getName()).log(Level.SEVERE, "IOException", ex);
+	    }
+	    
+	    return credentials;
+	}
+	
+	public static int publish(String topic, String messageContents) {
+		
+	  String topicNameString = topic;
+	  String messageString = messageContents;
+	
+	  // Setup google credentials 
+	
+	  GoogleCredentials credentials = PlasmaPubSubTools.getCredentials();
+		  
+	  // Set project name & topic name 
+	  ProjectTopicName topicName = ProjectTopicName.of("idyllic-kit-191017", topicNameString);
+	  Publisher publisher = null;
+	  
+	  // Initialize the lists 
+	  List<ApiFuture<String>> messageIdFutures = new ArrayList<>();
+	  List<String> messageIds = null;
+
+	  try {
+	    // Create a publisher instance bound to the topic
+	    try {
+			publisher = Publisher.newBuilder(topicName).setCredentialsProvider(FixedCredentialsProvider.create(credentials)).build();
+		} catch (IOException ex) {
+			 Logger.getLogger(PlasmaPubSubTools.class.getName()).log(Level.SEVERE, "IOException", ex);
+			 ex.printStackTrace();
+			 return -1;
+
+		}
+
+	    List<String> messages = Arrays.asList(messageString);
+
+	    // Schedule publishing one message at a time : messages get automatically batched
+	    for (String message : messages) {
+	      ByteString data = ByteString.copyFromUtf8(message);
+	      PubsubMessage pubsubMessage = PubsubMessage.newBuilder().setData(data).build();
+
+	      // Once published, returns a server-assigned message id (unique within the topic)
+	      ApiFuture<String> messageIdFuture = publisher.publish(pubsubMessage);
+	      messageIdFutures.add(messageIdFuture);
+	    }
+	    
+	  } finally {
+	    // wait on any pending publish requests.
+		  
+		try {
+			messageIds = ApiFutures.allAsList(messageIdFutures).get();
+		} catch (InterruptedException ex) {
+			Logger.getLogger(PlasmaPubSubTools.class.getName()).log(Level.SEVERE, "Execution exception", ex);
+			 ex.printStackTrace();
+			 return -1;
+		} catch (ExecutionException ex) {
+			Logger.getLogger(PlasmaPubSubTools.class.getName()).log(Level.SEVERE, "Execution exception", ex);
+			 ex.printStackTrace();
+			 return -1;
+		}
+
+	    for (String messageId : messageIds) {
+	      System.out.println("published with message ID: " + messageId);
+	    }
+
+	    if (publisher != null) {
+	      // When finished with the publisher, shutdown to free up resources.
+	      try {
+			publisher.shutdown();
+		} catch (Exception ex) {
+			Logger.getLogger(PlasmaPubSubTools.class.getName()).log(Level.SEVERE, "Exception", ex);
+			ex.printStackTrace();
+			return -1;
+		}
+	    }
+	  }
+	
+	return 0;
+	}
 
 	public static void main(String[] args) {
 		
-		String topicNameString = "reddit_topic";
-		String messageString = "Hello World!";
-		
-		// Setup google credentials 
-		
-		GoogleCredentials credentials = null;
-		  File credentialsPath = new File("C:\\PersonalWork\\files\\pubsub_key.json");  // Key path
-		  try (FileInputStream serviceAccountStream = new FileInputStream(credentialsPath)) {
-		      credentials = ServiceAccountCredentials.fromStream(serviceAccountStream);
-		  } catch (FileNotFoundException ex) {
-			  Logger.getLogger(PlasmaPubSubTools.class.getName()).log(Level.SEVERE, "File not found", ex);
-		  } catch (IOException ex) {
-			  Logger.getLogger(PlasmaPubSubTools.class.getName()).log(Level.SEVERE, "IOException", ex);
-		  }
-		  
-		  // Set project name & topic name 
-		  ProjectTopicName topicName = ProjectTopicName.of("idyllic-kit-191017", topicNameString);
-		  Publisher publisher = null;
-		  
-		  // Initialize the lists 
-		  List<ApiFuture<String>> messageIdFutures = new ArrayList<>();
-		  List<String> messageIds = null;
-
-		  try {
-		    // Create a publisher instance bound to the topic
-		    try {
-				publisher = Publisher.newBuilder(topicName).setCredentialsProvider(FixedCredentialsProvider.create(credentials)).build();
-			} catch (IOException ex) {
-				 Logger.getLogger(PlasmaPubSubTools.class.getName()).log(Level.SEVERE, "IOException", ex);
-				 ex.printStackTrace();
-
-			}
-
-		    List<String> messages = Arrays.asList(messageString);
-
-		    // Schedule publishing one message at a time : messages get automatically batched
-		    for (String message : messages) {
-		      ByteString data = ByteString.copyFromUtf8(message);
-		      PubsubMessage pubsubMessage = PubsubMessage.newBuilder().setData(data).build();
-
-		      // Once published, returns a server-assigned message id (unique within the topic)
-		      ApiFuture<String> messageIdFuture = publisher.publish(pubsubMessage);
-		      messageIdFutures.add(messageIdFuture);
-		    }
-		    
-		  } finally {
-		    // wait on any pending publish requests.
-			  
-			try {
-				messageIds = ApiFutures.allAsList(messageIdFutures).get();
-			} catch (InterruptedException ex) {
-				Logger.getLogger(PlasmaPubSubTools.class.getName()).log(Level.SEVERE, "Execution exception", ex);
-				 ex.printStackTrace();
-			} catch (ExecutionException ex) {
-				Logger.getLogger(PlasmaPubSubTools.class.getName()).log(Level.SEVERE, "Execution exception", ex);
-				 ex.printStackTrace();
-			}
-
-		    for (String messageId : messageIds) {
-		      System.out.println("published with message ID: " + messageId);
-		    }
-
-		    if (publisher != null) {
-		      // When finished with the publisher, shutdown to free up resources.
-		      try {
-				publisher.shutdown();
-			} catch (Exception ex) {
-				Logger.getLogger(PlasmaPubSubTools.class.getName()).log(Level.SEVERE, "Exception", ex);
-				 ex.printStackTrace();
-			}
-		    }
-		  }
+		PlasmaPubSubTools.publish("reddit_topic", "Blah!");
 
 	}
 
