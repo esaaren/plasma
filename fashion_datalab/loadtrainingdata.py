e5fd3c97615fb7e61e46b2dbc1d033f30fdca5a0
Erik <erik.saarenvirta93@gmail.com>
2019-02-11 13:49:21 -0500

    Update loadtrainingdata.py

--- fashion_datalab/loadtrainingdata.py
+++ fashion_datalab/loadtrainingdata.py
@@ -1,7 +1,6 @@
-
 # coding: utf-8
 
-# In[1]:
+# In[2]:
 
 
 import glob
@@ -12,6 +11,8 @@ import google.datalab.storage as storage
 import pickle
 import re
 import nltk
+from PIL import Image
+import matplotlib.pyplot as plt
 from nltk.corpus import stopwords 
 from nltk.tokenize import word_tokenize 
 from collections import Counter
@@ -23,12 +24,22 @@ from tensorflow.python.saved_model import builder as saved_model_builder
 from tensorflow.python.saved_model import tag_constants, signature_constants
 from tensorflow.python.saved_model.signature_def_utils_impl import predict_signature_def
 from tensorflow.python.lib.io import file_io
+from tensorflow.keras.preprocessing.image import load_img, array_to_img
+
+
+import keras
+from keras import backend as K
+from keras import layers, models
+from keras.utils import np_utils
+from keras.models import Sequential
+from keras.layers.core import Dense, Activation
+from keras.optimizers import SGD
 
 import numpy as np
 from io import BytesIO
 
 
-# In[7]:
+# In[3]:
 
 
 # Get Cleaned File Dict 
@@ -36,7 +47,7 @@ with open('clean_file_dict.pickle', 'rb') as handle:
   clean_file_dict = pickle.load(handle)
 
 
-# In[3]:
+# In[4]:
 
 
 # Get Common Word List 
@@ -44,7 +55,7 @@ with open('common_word_list.pickle', 'rb') as handle:
   common_word_list = pickle.load(handle)
 
 
-# In[12]:
+# In[5]:
 
 
 # Define the bucket location
@@ -97,19 +108,23 @@ with sess.as_default():
     for each_image in image_list:
       image = tf.read_file(os.path.join(full_path + '/', each_image))
       image = tf.image.decode_jpeg(image,channels=3)
-      resized_image = tf.image.resize_images(image, [64, 64])
+      # Important to divide by 255 when plotting for matplot lib because the tf image is 0-->255, this will also normalize data from 0 --> 1
+      image = tf.cast(image, tf.float32) / 255.0
+      resized_image = tf.image.resize_images(image, [88, 59])
       image_array = sess.run(resized_image)
       image_array_flat = image_array.flatten()
+      
+      
       X_list.append(image_word_vector)
       Y_list.append(image_array_flat)
   
-    inc = inc + 1
-    
     if (inc % 100) == 0:
       print "Loaded {} images".format(inc)
       
-    if inc > 1000:
+    if inc > 100:
       break
+
+    inc = inc + 1
 
 
 X = np.asarray(X_list)
@@ -122,7 +137,28 @@ with open('training_output.pickle', 'wb') as handle:
   pickle.dump(Y, handle, protocol=pickle.HIGHEST_PROTOCOL)
 
 
-# In[ ]:
+# In[6]:
+
+
+# Optional to read in the inputs from before
+
+# Get labelled input  
+with open('training_input.pickle', 'rb') as handle:
+  X = pickle.load(handle)
+  
+  # Get labelled output
+with open('training_output.pickle', 'rb') as handle:
+  Y = pickle.load(handle)
+
+
+# In[7]:
+
+
+print X.shape
+print Y.shape
+
+
+# In[16]:
 
 
 # Build the keras model now 
@@ -130,14 +166,33 @@ with open('training_output.pickle', 'wb') as handle:
 model = Sequential()
 
 input_size = len(common_word_list)
-output_size = 64*64*3
+output_size = 88*59*3
 hidden_size = (input_size + output_size) / 2
 
 
-model.add(Dense(input_size, activation="tanh", kernel_initializer="uniform", input_dim=input_size))   # input neuron expecting dimension 1
-model.add(Dense(hidden_size, activation="sigmoid", kernel_initializer="uniform"))                # hidden layer neurons
-model.add(Dense(output_size, kernel_initializer="uniform"))
+model.add(Dense(input_size, activation="relu", input_dim=input_size))   # input neuron expecting dimension 1
+model.add(Dense(hidden_size, activation="relu"))                # hidden layer neurons
+model.add(Dense(output_size, activation="sigmoid"))
 
-model.compile(loss='mse', optimizer=SGD(lr=0.1, momentum=0.99, decay=0.001, nesterov=False))
+model.compile(loss='mse', optimizer=SGD(lr=0.9, momentum=0.99, decay=0.001, nesterov=False))
 
-model.fit(X, Y, epochs=10, batch_size=100, validation_split=0.2, verbose=0)
+model.fit(X, Y, epochs=150, batch_size=10, validation_split=0.2, verbose=0)
+
+
+# In[13]:
+
+
+X_predict = np.zeros([1,636])
+X_predict[0] = X[0]
+prediction = model.predict(X_predict)
+
+
+# In[14]:
+
+
+predict_array = prediction[0].reshape(88,59,3)
+print predict_array
+print Y[0]
+
+plt.imshow(predict_array)
+
